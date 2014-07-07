46b1697e93fcaf635ca1429dafc752e44469ad45
Rodrigo Matheus <rodrigorizando@gmail.com>
2014-07-07 10:51:08 -0300

    fixed bug on generate letters parameters

---
+++ test.js
@@ -0,0 +1,21 @@
+var crawlerJS = require('./index.js');
+
+var worlds = {
+  limiter: 1,
+  interval: 1000,
+  getSample: 'http://www.tibia.com/community/?subtopic=worlds',
+  get: 'http://www.tibia.com/community/?subtopic=[letters:2]',
+  statusHeader: [200],
+  block: ['your ip is blocked'],
+  preview: 1,
+  extractors: [
+    {
+      dataType: '0',
+      selector: '.TableContentContainer table.TableContent tr',
+      elements: "data.world = $(this).children('td').eq(0).children('a').attr('href'); if(typeof data.world == 'undefined'){delete data.world;}",
+      csv: 'worlds.csv'
+    }
+  ]
+}
+
+crawlerJS(worlds)
